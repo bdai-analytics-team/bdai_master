b1887229e5c1490eca1f8c3d79ef3c3ba557e4b2
Forlefac Fontem <fontem.2@osu.edu>
2019-09-15 13:39:46 -0400

    added methods for getting stocks from iex

--- pipeline/IEXScraper.py
+++ pipeline/IEXScraper.py
@@ -1,5 +1,24 @@
 from iexfinance.stocks import get_historical_data
+from iexfinance.stocks import Stock
+from datetime import date, timedelta
 from iexfinance.refdata import get_symbols
+
+
+def get_last_business_day(adate):
+    adate -= timedelta(days=1)
+    while adate.weekday() > 4:  # Mon-Fri are 0-4
+        adate -= timedelta(days=1)
+    return adate
+
+
+def get_stocks_last_days_data(stock_name):
+    start = get_last_business_day(date.today())
+    return get_historical_data(
+        stock_name, start, end=None, output_format='pandas')
+
+
+print(get_stocks_last_days_data('AAPL'))
+
 
 def get_ticker_list(file_name):
     nyse_file = open(file_name)
@@ -16,8 +35,10 @@ def get_ticker_list(file_name):
 
     return target_tickers
 
-#TODO: Make a separate file for helper methods like these two
-#Takes python date and returns a string formatted in the manner used by IEX in JSON
+# TODO: Make a separate file for helper methods like these two
+# Takes python date and returns a string formatted in the manner used by IEX in JSON
+
+
 def format_date(date):
     year = str(date.year)
     month = str(date.month)
@@ -28,7 +49,8 @@ def format_date(date):
         month = '0' + month
     return year + '-' + month + '-' + day
 
+
 def get_month_length(month, year):
-    month_lengths = {1 : 31, 2 : 28 + int(year / 4), 3 : 31, 4 : 30, 5 : 31, 6 : 30,
-                     7 : 31, 8 : 31, 9 : 30, 10 : 31, 11 : 30, 12 : 31}
+    month_lengths = {1: 31, 2: 28 + int(year / 4), 3: 31, 4: 30, 5: 31, 6: 30,
+                     7: 31, 8: 31, 9: 30, 10: 31, 11: 30, 12: 31}
     return month_lengths[month]
