e771d0a61f86a40bcdbc592561697e4b16344477
Ankit Deogharia <ankit.deogharia@gmail.com>
2019-10-27 14:12:21 -0400

    Updated docs and setup scripts

--- lambda_functions/alpaca-sell/setup.sh
+++ lambda_functions/alpaca-sell/setup.sh
@@ -1,6 +1,4 @@
-# Requires that Node and NPM are already installed
+#Requires that Node and NPM are already installed
 npm i -g serverless
 sls --version
 pip install pipenv
-pipenv shell
-pipenv install --dev
